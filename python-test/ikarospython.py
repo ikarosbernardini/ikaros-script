018125c6dfef681d57badf161ecdfee6d1b93bb2
Ikaros Bernardini <ikaros.bernardini@gmail.com>
2025-08-27 16:58:16 +0200

    test

--- python-test/ikarospython.py
+++ python-test/ikarospython.py
@@ -3,4 +3,13 @@
 x = 10
 print(x)
 y = 5
+print('Hello, world!')
+print('What is your name?')  # Ask for their name.
+my_name = input('>')
+print('It is good to meet you, ' + my_name)
+print('The length of your name is:')
+print(len(my_name))
+print('What is your age?')  # Ask for their age.
+my_age = input('>')
+print('You will be ' + str(int(my_age) + 1) + ' in a year.')
 
---
+++ python-test/nackademin.py
@@ -0,0 +1,13 @@
+def greet():
+    print("Hello, welcome to Nackademin!")
+print("----------------")
+greet()  
+print('----------------')
+
+def Count():
+    for i in range(1, 11):
+        print(i)
+
+# Call the function
+Count()
+greet("name")
